dfb50f4e6ec2d41c83b8aea050ae2162b1c08f56
Mohamed Diop <yxngbizzy@gmail.com>
2021-10-23 23:20:51 -0700

    responsive design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import logo from './logo.svg';
 import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
+import moment from 'moment'
 import contractConfiguration from './utils/HelloMo.json'
 import './App.css';
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState('')
-  const [helloCount, setHelloCount] = useState()
-
-  const contractAddress = '0x1DA8eF4aDB9E22BEcB5a5f0B8B615b62a8982073'
+  const [allHellos, setAllHellos] = useState([])
+  const contractAddress = '0xe598d038002B1aaE57313661701b000e6B609f3a'
   const contractABI = contractConfiguration.abi
+  const [helloCount, setHelloCount] = useState()
 
   const checkIfWalletIsConnected = () => {
     try {
@@ -22,13 +23,14 @@ function App() {
         console.log("Make sure you have metamask!");
         return;
       } else {
-        console.log("We have the ethereum object", ethereum);
+        console.log("We have the ethereum object");
       }
       const accounts = ethereum.request({method: 'eth_accounts'})
     
       if(accounts.length !== 0) {
         console.log('Found a wallet', accounts[0])
         setCurrentAccount(accounts[0])
+        getAllHellos()
       } else {
         console.log('No wallet found')
       }
@@ -59,7 +61,8 @@ function App() {
         const signer = provider.getSigner()
         const Contract = new ethers.Contract(contractAddress, contractABI, signer)
 
-        const Txn = await Contract.sayHello();
+        const input = document.querySelector('input')
+        const Txn = await Contract.sayHello(input.value, { gasLimit: 300000 });
         console.log("Mining...", Txn.hash);
 
         await Txn.wait();
@@ -68,6 +71,7 @@ function App() {
         let count = await Contract.getTotalHellos()
         console.log("Retrieved total wave count...", count.toNumber());
         setHelloCount(count.toNumber())
+        getAllHellos()
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -76,14 +80,45 @@ function App() {
     }
   }
 
-  /*
-  * This runs our function when the page loads.
-  */
+  const getAllHellos = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const Contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const hellos = await Contract.getAllHellos();
+
+        let hellosCleaned = [];
+        hellos.forEach(hello => {
+          hellosCleaned.push({
+            address: hello.from,
+            timestamp: new Date(hello.timestamp * 1000),
+            message: hello.message
+          });
+        });
+        Contract.on('NewHello', (from, timestamp, message) => {
+          hellosCleaned.push({
+            address: from,
+            timestamp: new Date(timestamp * 1000),
+            message: message
+          });
+        })
+
+        setAllHellos(hellosCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(checkIfWalletIsConnected)
   
   return (
-    <div className="mainContainer">
-
+  <div className="mainContainer">
     <div className="dataContainer">
       <div className="header">
       👋 Hey there!
@@ -92,15 +127,27 @@ function App() {
       <div className="bio">
       Connect your wallet and say hello!
       </div>
-
-      <button className="helloButton" onClick={sayHello}>
-        Hello
-      </button>
+      <div class='input-box'>
+        <input type='text'/>
+        <button className="helloButton" onClick={sayHello}>
+          Hello
+        </button>
+      </div>
       {!currentAccount && 
       <button className="helloButton" onClick={connectWallet}>
         Connect Wallet
       </button>
       }
+      <div class='posts'>
+      {allHellos.map((hello, index) => {
+          return (
+            <div class='post' key={index}>
+              <p class='message'>{hello.message}</p>
+              <p class='address'>From {hello.address}</p>
+              <p class='date'>{moment(hello.timestamp).format('LLLL')}</p>
+            </div>)
+        })}
+      </div>
     </div>
   </div>
   );
